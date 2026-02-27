c66c4542b07fc43e4e6c68e6c7682889976e8f9e
noaharaki <145617728+noaharaki@users.noreply.github.com>
2026-02-26 17:50:40 -0800

    Update server_config.py

--- server/server_config.py
+++ server/server_config.py
@@ -1,1 +1,6 @@
+SERVER_CONFIG = [
+    {"id": 0, "host": "localhost", "port": 5001, "capacity": 100},
+    {"id": 1, "host": "localhost", "port": 5002, "capacity": 80},
+    {"id": 2, "host": "localhost", "port": 5003, "capacity": 120},
+]
 
